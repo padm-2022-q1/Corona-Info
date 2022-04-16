74c0c891ec2a13117e37436355783af413f8584a
Rafael Mori <hajimemori97@gmail.com>
2022-04-16 13:51:48 -0300

    Layout o que é SARS Cov 2

---
+++ app/src/main/java/br/edu/ufabc/corona_info/SarsCov2InfoFragment.kt
@@ -0,0 +1,11 @@
+package br.edu.ufabc.corona_info
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SarsCov2InfoFragment : Fragment() {
+
+}
