09ce273ba2474c3f8b6b1f9c35e4feaf44bc610e
Matheus Augusto <mts.013@hotmail.com>
2022-04-16 14:53:04 -0300

    Implementacao do startup da HomeFragment

--- app/src/main/java/br/edu/ufabc/corona_info/HomeFragment.kt
+++ app/src/main/java/br/edu/ufabc/corona_info/HomeFragment.kt
@@ -1,7 +1,22 @@
 package br.edu.ufabc.corona_info
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import br.edu.ufabc.corona_info.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return  binding.root
+    }
 }
---
+++ app/src/main/java/br/edu/ufabc/corona_info/MainViewModel.kt
@@ -0,0 +1,11 @@
+package br.edu.ufabc.corona_info
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Main ViewModel.
+ */
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
