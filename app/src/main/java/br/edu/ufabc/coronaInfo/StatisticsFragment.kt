90e24bf390905135b46cb47f32dbae22564cd178
Matheus Augusto <mts.013@hotmail.com>
2022-04-17 22:30:34 -0300

    Estilizacao do Spinner

--- app/src/main/java/br/edu/ufabc/coronaInfo/StatisticsFragment.kt
+++ app/src/main/java/br/edu/ufabc/coronaInfo/StatisticsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import br.edu.ufabc.coronaInfo.databinding.FragmentStatisticsBinding
 
@@ -16,6 +17,21 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        adapterSpinner()
         return binding.root
+    }
+
+    private fun adapterSpinner() {
+        val spinner = binding.statisticsDropdownList
+        activity?.let {
+            ArrayAdapter.createFromResource(
+                it.baseContext,
+                R.array.states_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+        }
     }
 }
